c29eb490e5b007590a47931fdf661384f5511b82
Manv3ndra <72267209+Manv3ndra@users.noreply.github.com>
2022-02-05 10:28:06 +0530

    Day 16 commit

--- Day 16/miles_to_kilometeres.py
+++ Day 16/miles_to_kilometeres.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 def miles_to_km():
     miles = float(miles_input.get())
-    km = round(miles * 1.609)
-    kilometer_result_label.config(text = f"{km}")
+    km = miles * 1.609
+    kilometer_result_label.config(text = "%.2f" %km)
 
 window = Tk()
 window.title("Miles to Km")
@@ -14,14 +14,14 @@ miles_input = Entry(width=7)
 miles_input.grid(column=1, row=0)
 
 #Miles Label
-miles_label = Label(text="Miles", font=("Arial", 20, "bold"))
-miles_input.grid(column=2, row=0)
+miles_label = Label(text="Miles", font=("Arial", 20))
+miles_label.grid(column=2, row=0)
 
 #Is Equal To Label
-isequalto_label = Label(text="is equal to", font=("Arial", 20, "bold"))
-miles_input.grid(column=0, row=1)
+isequalto_label = Label(text="is equal to", font=("Arial", 20))
+isequalto_label.grid(column=0, row=1)
 
-kilometer_result_label = Label(text="0", font=("Arial", 20, "bold"))
+kilometer_result_label = Label(text="0", font=("Arial", 20))
 kilometer_result_label.grid(column=1, row=1)
 
 calculate_button = Button(text="Calculate", command=miles_to_km)
