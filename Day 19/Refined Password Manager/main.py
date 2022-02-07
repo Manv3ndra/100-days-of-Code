dbb1c837bf7348658ff6892593b2665d1685dccc
Manv3ndra <72267209+Manv3ndra@users.noreply.github.com>
2022-02-08 02:12:35 +0530

    Day 19 commit

--- Day 19/Refined Password Manager/main.py
+++ Day 19/Refined Password Manager/main.py
@@ -63,12 +63,18 @@ def save_password():
 # Search Password
 def find_password():
     website = website_entry.get()
-    with open("data.json", mode="r") as f:
-        data = json.load(f)
+    try:
+        with open("data.json", mode="r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
         if website in data:
-            email = data[website]["email"]
-            password = data[website]["password"]
-            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists")
 
 # UI Setup
 window = Tk()
