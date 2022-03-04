e4552afe72f2a359e302ffbbaee31c37e5ffb465
Manv3ndra <72267209+Manv3ndra@users.noreply.github.com>
2022-03-04 05:33:10 +0530

    Day 32 commit

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,14 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
 # Scraping Billboard 100
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 soup = BeautifulSoup(response.text, 'html.parser')
-song_names_spans = soup.find_all("span", class_="chart-element__information__song")
-song_names = [song.getText() for song in song_names_spans]
+song_names_spans = soup.find("div", class_="chart-results-list // lrv-u-padding-t-150 lrv-u-padding-t-050@mobile-max")
+tag = song_names_spans.find_all("div", class_="o-chart-results-list-row-container")
+
+song_names = []
+
+top = soup.find("a", class_="c-title__link lrv-a-unstyle-link").get_text().strip()
+
+for name in tag:
+    try:
+        song = name.find("h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only").get_text().strip()
+        song_names.append(song)
+    except AttributeError:
+        song_names.append(top)
 
 #Spotify Authentication
 sp = spotipy.Spotify(
@@ -22,14 +33,14 @@ sp = spotipy.Spotify(
     )
 )
 user_id = sp.current_user()["id"]
-print(user_id)
+# print(user_id)
 
 #Searching Spotify for songs by title
 song_uris = []
-year = date.split("-")[0]
+# year = date.split("-")[0]
 for song in song_names:
-    result = sp.search(q=f"track:{song} year:{year}", type="track")
-    print(result)
+    result = sp.search(q=song, type="track")
+    # print(result)
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
@@ -37,8 +48,9 @@ for song in song_names:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
 #Creating a new private playlist in Spotify
-playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
-print(playlist)
+playlist = sp.user_playlist_create(user=user_id, name=f"Billboard Top 100 {date}", public=False)
+# print(playlist)
 
 #Adding songs found into the new playlist
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+print("Created and Added Songs to Playlist")
